1e118cfa6fae35e57f06115f945ad8596ba342a7
Alexis Cuevas Camarena <filianenjoyer@gmail.com>
2025-10-20 15:27:59 -0700

    Agregar archivos de proyecto.

---
+++ InveDB/Datos/ContextoAlmacen.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using InveDB.Modelos;
+namespace InveDB.Datos
+{
+    public class ContextoAlmacen : DbContext
+    {
+        public ContextoAlmacen(DbContextOptions<ContextoAlmacen> options) : base(options) { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<Sucursal> Sucursales { get; set; }
+        public DbSet<Inventario> Inventarios { get; set; }
+        public DbSet<Movimiento> Movimientos { get; set; }
+    }
+}
---
+++ InveDB/Program.cs
@@ -0,0 +1,52 @@
+﻿using InveDB.Datos;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ---------------------------
+// 1️ Registrar servicios
+// ---------------------------
+
+// Registrar el contexto de base de datos con la cadena de conexión
+builder.Services.AddDbContext<ContextoAlmacen>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<EjecutarCmdWeb>();
+
+
+
+// Agregar soporte para controladores y vistas MVC
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
+var app = builder.Build();
+
+// ---------------------------
+// 2️ Configurar la aplicación
+// ---------------------------
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts(); // Activa seguridad HTTPS
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+// ---------------------------
+// 3️ Configurar rutas MVC
+// ---------------------------
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Inicio}/{action=Index}/{id?}");
+
+// ---------------------------
+// 4️ Ejecutar la aplicación
+// ---------------------------
+app.Run();
